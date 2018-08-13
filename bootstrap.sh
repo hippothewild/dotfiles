c3c8b4c3438a089a5508c0d387c9a997f9cd2812
Jihwan Chun <jihwan0321@gmail.com>
2018-08-13 21:51:08 +0900

    Added VSCode configuration.

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,7 @@ brew bundle --file=$DOTFILE_DIR/Brewfile
 brew cleanup
 brew cask cleanup
 
-# Setup git configuration
+# Set git configuration
 [ ! -f $HOME/.gitconfig ] && ln -nfs $DOTFILE_DIR/gitconfig $HOME/.gitconfig
 
 # Change default shell to zsh, install oh-my-zsh and set configuration
@@ -36,3 +36,15 @@ chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 [ ! -f $HOME/.zshrc ] && ln -nfs $DOTFILE_DIR/zshrc $HOME/.zshrc
 source $HOME/.zshrc
+
+# Install Visual Studio Code extensions
+if test $(which code); then
+  code --install-extension eg2.tslint
+  code --install-extension mauve.terraform
+  code --install-extension ms-vscode.Go
+  code --install-extension octref.vetur
+  code --install-extension PeterJausovec.vscode-docker
+
+  cp $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.backup.json
+  cp ./vscode_settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+fi
